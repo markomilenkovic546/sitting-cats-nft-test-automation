1118bac60304d06f59ee83658496210b6797f620
markomilenkovic546 <markomilenkovic546@gmail.com>
2023-09-29 14:18:56 +0200

    code cleanup and bug fixing

--- tests/e2e/specs/01-test-accounts-setup.js
+++ tests/e2e/specs/01-test-accounts-setup.js
@@ -1,13 +1,11 @@
+// Import Metamask accounts
 it("Import MetaMask accounts", function () {
-
   for (let i = 0; i < 8; i++) {
-    cy.createMetamaskAccount().then(created => {
-        expect(created).to.be.true;
-      });
+    cy.createMetamaskAccount().then((created) => {
+      expect(created).to.be.true;
+    });
   }
-  cy.switchMetamaskAccount('account 1').then(switched => {
+  cy.switchMetamaskAccount("account 1").then((switched) => {
     expect(switched).to.be.true;
   });
-  
- 
-  });
+});
--- tests/e2e/support.js
+++ tests/e2e/support.js
@@ -1,59 +1,61 @@
-import '@synthetixio/synpress/support/index';
+import "@synthetixio/synpress/support/index";
 const ethers = require("ethers");
 const abi = require("./abi.json");
 
-Cypress.Commands.add('formatAddress', (address) => {
-    const firstPart = address.slice(0, 4);
-    const lastPart = address.slice(-4);
-    const shortenedAddress = `${firstPart}...${lastPart}`;
-    return shortenedAddress;
- })
+// Format public address acording to profile button design
+Cypress.Commands.add("formatAddress", (address) => {
+  const firstPart = address.slice(0, 4);
+  const lastPart = address.slice(-4);
+  const shortenedAddress = `${firstPart}...${lastPart}`;
+  return shortenedAddress;
+});
 
+// Get allowed number of NFTs to mint according to current user's claim status
+Cypress.Commands.add("getAllowedNumberToMint", (text) => {
+  const numbers = text.match(/\d+/g);
+  const firstNumber = numbers[0];
+  const allowedNumber = 7 - Number(firstNumber);
+  return allowedNumber;
+});
 
- Cypress.Commands.add('getAllowedNumberToMint', (text) => {
-    const numbers = text.match(/\d+/g); 
-    const firstNumber = numbers[0];
-    const allowedNumber = 7 - Number(firstNumber)
-    return allowedNumber
- })
-
- // To call the function on smart contract to get owner of the NFT 
+// To call the function on smart contract to get the NFT owner
 async function getNftOwner(nftId) {
-   const contractAddress = Cypress.env("scAddress");
-   const provider = new ethers.providers.WebSocketProvider(Cypress.env("providerURL"))
-   const contract = new ethers.Contract(contractAddress, abi, provider);
-   try {
-     const readOwner = await contract.ownerOf(nftId);
-     console.log(readOwner);
-     console.log(typeof readOwner);
-     return readOwner;
-   } catch (error) {
-     console.error(error);
-     return readOwner
-   }
- }
- 
-// Call the function on smart contract to get owner of the NFT by passing NFT id as an parameter
- Cypress.Commands.add('getNftOwner', (nftId) => {
-   return getNftOwner(nftId);
- });
- 
+  const contractAddress = Cypress.env("scAddress");
+  const provider = new ethers.providers.WebSocketProvider(Cypress.env("providerURL"));
+  const contract = new ethers.Contract(contractAddress, abi, provider);
+  try {
+    const readOwner = await contract.ownerOf(nftId);
+    console.log(readOwner);
+    console.log(typeof readOwner);
+    return readOwner;
+  } catch (error) {
+    console.error(error);
+    return readOwner;
+  }
+}
 
- async function getTotalMintedValue() {
-   const contractAddress = Cypress.env("scAddress");
-   const provider = new ethers.providers.WebSocketProvider(Cypress.env("providerURL"))
-   const contract = new ethers.Contract(contractAddress, abi, provider);
-   try {
-     const totalMintedValue = await contract.totalMinted();
-     const bigNumber = ethers.BigNumber.from(totalMintedValue);
-     const stringValue = bigNumber.toString();
-     return stringValue; // Return the string value
-   } catch (error) {
-     console.error(error);
-     return null; // Handle the error and return an appropriate value
-   }
- }
+// Call the function on smart contract to get NFT owner by passing NFT id as an parameter
+Cypress.Commands.add("getNftOwner", (nftId) => {
+  return getNftOwner(nftId);
+});
 
- Cypress.Commands.add('getTotalClaimedValue', (nftId) => {
-   return getTotalMintedValue();
- });
+// To call the function on smart contract to get total claimed info
+async function getTotalMintedValue() {
+  const contractAddress = Cypress.env("scAddress");
+  const provider = new ethers.providers.WebSocketProvider(Cypress.env("providerURL"));
+  const contract = new ethers.Contract(contractAddress, abi, provider);
+  try {
+    const totalMintedValue = await contract.totalMinted();
+    const bigNumber = ethers.BigNumber.from(totalMintedValue);
+    const stringValue = bigNumber.toString();
+    return stringValue;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+// Call the function on smart contract to get total claimed info
+Cypress.Commands.add("getTotalClaimedValue", (nftId) => {
+  return getTotalMintedValue();
+});
